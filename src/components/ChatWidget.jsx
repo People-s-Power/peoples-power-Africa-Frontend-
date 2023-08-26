87d7c6b72df3bf7beb68c2b23d47ccda4b457734
alabo-excel <iamalaboexcel@gmail.com>
2023-08-26 06:40:42 +0100

    chatwidget update commit

--- src/components/ChatWidget.jsx
+++ src/components/ChatWidget.jsx
@@ -3,43 +3,55 @@ import { UserAtom } from "atoms/UserAtom"
 import { useRecoilValue } from "recoil"
 
 const ChatWidget = () => {
-  const author = useRecoilValue(UserAtom)
+  const user = useRecoilValue(UserAtom)
   const crypto = require("crypto");
 
   const key = "nj4Rk7eeLMDVGMuRWokrcLxE";
   // const message = "some-unique-identifier";
 
   // Generate the HMAC
-  const identifierHash = crypto
-    .createHmac("sha256", key)
-    .update("the-plaint-org")
-    .digest("hex");
+
 
   useEffect(() => {
-    // Add Chatwoot Settings
-    window.chatwootSettings = {
-      hideMessageBubble: false,
-      position: 'right', // This can be left or right
-      locale: 'en', // Language to be set
-      type: 'standard', // [standard, expanded_bubble]
-    };
+    if (user) {
+      console.log(user)
+      const identifierHash = crypto
+        .createHmac("sha256", key)
+        .update(user.id)
+        .digest("hex");
 
-    (function (d, t) {
-      var BASE_URL = "https://app1.chatcloud.ai";
-      var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-      g.src = 'https://chatcloud.b-cdn.net' + "/packs/js/sdk.js";
-      g.defer = true;
-      g.async = true;
-      s.parentNode.insertBefore(g, s);
-      g.onload = function () {
-        window.chatcloudSDK.run({
-          websiteToken: 'upiFuPTagEtjLCngpGwg9t7c',
-          baseUrl: BASE_URL,
-          identifier_hash: identifierHash,
-        })
-      }
-    })(document, "script");
-  }, [])
+      window.chatwootSettings = {
+        hideMessageBubble: false,
+        position: 'right', // This can be left or right
+        locale: 'en', // Language to be set
+        type: 'standard', // [standard, expanded_bubble]
+      };
+
+      (function (d, t) {
+        var BASE_URL = "https://app1.chatcloud.ai";
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = 'https://chatcloud.b-cdn.net' + "/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g, s);
+        g.onload = function () {
+          window.chatcloudSDK.run({
+            websiteToken: 'upiFuPTagEtjLCngpGwg9t7c',
+            baseUrl: BASE_URL,
+            // identifier_hash: identifierHash,
+          })
+          window.$chatcloud.setUser(user.id, {
+            name: user.name,
+            avatar_url: user.image,
+            email: user.email,
+            identifier_hash: identifierHash,
+            description: user.description,
+            city: user.city,
+          });
+        }
+      })(document, "script");
+    }
+  }, [user])
 
 
   return null;
