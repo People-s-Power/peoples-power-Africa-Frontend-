d28085a3c77a512d75476902139e161b3a337207
alabo-excel <iamalaboexcel@gmail.com>
2023-09-04 08:51:57 +0100

    corrections

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -213,13 +213,13 @@ const Single = ({ repo, }: InferGetServerSidePropsType<typeof getServerSideProps
         <meta property="og:type" content="website" />
         <meta property="og:title" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name} />
         <meta property="og:description" content={repo?.body} />
-        <meta property="og:image" content={repo?.asset[0]} />
+        <meta property="og:image" content={repo?.asset[0].url} />
         <meta property="og:url" content="https://theplaint.org" />
         <meta property="og:site_name" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name} />
 
         <meta name="twitter:title" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name} />
         <meta name="twitter:description" content={repo?.body} />
-        <meta name="twitter:image" content={repo?.asset[0]} />
+        <meta name="twitter:image" content={repo?.asset[0].url} />
 
       </Head>
       <FrontLayout showFooter={false}>
--- src/pages/campaigns/[slug].tsx
+++ src/pages/campaigns/[slug].tsx
@@ -121,13 +121,13 @@ const SingleCampaignPage = ({ repo, }: InferGetServerSidePropsType<typeof getSer
 				<meta property="og:type" content="website" />
 				<meta property="og:title" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name} />
 				<meta property="og:description" content={repo?.body} />
-				<meta property="og:image" content={repo?.asset[0]} />
+				<meta property="og:image" content={repo?.asset[0].url} />
 				<meta property="og:url" content="https://theplaint.org" />
 				<meta property="og:site_name" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name}  />
 
 				<meta name="twitter:title" content={repo?.__typename + '||' + repo?.title || repo?.body || repo?.caption || repo?.name}  />
 				<meta name="twitter:description" content={repo?.body} />
-				<meta name="twitter:image" content={repo?.asset[0]} />
+				<meta name="twitter:image" content={repo?.asset[0].url} />
 			</Head>
 			<FrontLayout showFooter={false}>
 				<Wrapper className="single-camp py-4 ">
