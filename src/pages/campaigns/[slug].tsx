a63bb1f9ebc1410c0ac97803e3f954488be69bb1
alabo-excel <iamalaboexcel@gmail.com>
2023-09-07 10:33:44 +0100

    faceook share fix

--- src/pages/campaigns/[slug].tsx
+++ src/pages/campaigns/[slug].tsx
@@ -127,10 +127,10 @@ const SingleCampaignPage = ({ repo, }: InferGetServerSidePropsType<typeof getSer
 				<meta property="og:title" content={repo.title || repo.body || repo.caption || repo.name} />
 				<meta property="og:description" content={repo.body} />
 				<meta property="og:image" content={repo.asset[0].url} />
-				<meta property="og:url" content="https://theplaint.org" />
-				<meta property="og:site_name" content={repo.__typename + '||' + repo.title || repo.body || repo.caption || repo.name} />
+				<meta property="og:url" content={`https://theplaint.org/campaigns/${repo.slug}`} />
+				<meta property="og:site_name" content={repo.title || repo.body || repo.caption || repo.name} />
 
-				<meta name="twitter:title" content={repo.__typename + '||' + repo.title || repo.body || repo.caption || repo.name} />
+				<meta name="twitter:title" content={repo.title || repo.body || repo.caption || repo.name} />
 				<meta name="twitter:description" content={repo.body} />
 				<meta name="twitter:image" content={repo.asset[0].url} />
 
